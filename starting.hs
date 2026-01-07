b85d601a23277a075c04ecd6f1544f02377816b4
LeahTheSlug <LeahTheSlug@protonmail.com>
2026-01-07 15:25:25 -0600

    added another function in starting.hs

--- starting.hs
+++ starting.hs
@@ -4,4 +4,6 @@ doubleUs x y = doubleMe y + doubleMe x
 
 doubleSmallNumber x = if x > 100 
   then x 
-  else doubleMe x 
+  else doubleMe x
+
+boomBangs xs = [if x <10 then "BOOM!" else "BANG!" | x <- xs, odd x]
